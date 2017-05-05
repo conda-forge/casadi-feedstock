6dc9564e488bfafea3aef0e257af771378596c20
Peter St. John <peterc.stjohn@gmail.com>
2017-05-05 07:15:39 -0600

    Removing gfortran yum (#4)
    * removing yum-requirements and rerendering
    
    * bumping build number

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -57,14 +57,6 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-
-# Install the yum requirements defined canonically in the
-# "recipe/yum_requirements.txt" file. After updating that file,
-# run "conda smithy rerender" and this line be updated
-# automatically.
-/usr/bin/sudo -n yum install -y devtoolset-2-gcc-gfortran
-
-
 # Embarking on 3 case(s).
     set -x
     export CONDA_NPY=112
