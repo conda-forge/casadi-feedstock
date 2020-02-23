30a5c0c6587f1493aa2e1a882e5440dab762121b
Pariterre <pariterre@hotmail.com>
2020-02-23 11:08:09 -0500

    Cleaned the UNIX side

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,19 @@
-CMAKE_FLAG="-DWITH_PYTHON3=ON" WITH_PYTHON3=ON PY3=ON PSF=3 PYTHONVERSION=35
-
-if [ "$(uname)" == "Linux" ]
-then
-    # export CXXFLAGS="${CXXFLAGS} -L${PREFIX}/lib -lquadmath -lgfortran"
-    export CXXFLAGS="${CXXFLAGS} -L${PREFIX}/lib -Wl,-rpath-link,${PREFIX}/lib"
-fi
-
 mkdir build
 pushd build
 
 cmake $CMAKE_FLAG \
-  -DCMAKE_C_COMPILER=${CC} \
-  -DCMAKE_CXX_COMPILER=${CXX} \
-  -DWITH_SELFCONTAINED=OFF \
   -DWITH_PYTHON=ON \
+  -DWITH_PYTHON3=ON \
   -DWITH_LAPACK=ON \
   -DWITH_IPOPT=ON \
   -DWITH_JSON=ON \
-  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}\
+  -DCMAKE_INSTALL_PREFIX=${PREFIX}\
   -DPYTHON_PREFIX=${SP_DIR} \
   $SRC_DIR
 
 make VERBOSE=1 -j${CPU_COUNT}
 make install
+
+# Change a line of code that won't work with new version of python
+sed -i 's/_object/object/' $SP_DIR/casadi/casadi.py
+
