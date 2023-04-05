9d023a0ee370bab14dd2f301bde97d3c317549b2
Silvio Traversaro <silvio@traversaro.it>
2023-04-05 14:20:55 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,15 +10,13 @@ cmake ${CMAKE_ARGS} $SRC_DIR \
   -DWITH_JSON=ON \
   -DWITH_THREAD=ON \
   -DWITH_OSQP=ON \
+  -DWITH_BUILD_OSQP=OFF \
   -DWITH_QPOASES=ON \
   -DUSE_SYSTEM_WISE_OSQP=ON \
+  -DWITH_PROXQP=ON \
+  -DWITH_BUILD_PROXQP=OFF \
   -DPYTHON_PREFIX=${SP_DIR} \
   -DCASADI_PYTHON_PIP_METADATA_INSTALL=ON \
   -DCASADI_PYTHON_PIP_METADATA_INSTALLER:STRING="conda" \
   
 ninja install
-
-# Change a line of code that won't work with new version of python
-sed -i'.original' 's/_object/object/' $SP_DIR/casadi/casadi.py
-
-rm $SP_DIR/casadi/casadi.py.original 
