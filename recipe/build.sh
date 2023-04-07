246541b745c8dbe367a795b85ec5215605f69267
Silvio Traversaro <silvio@traversaro.it>
2023-04-07 15:21:01 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 mkdir build
 pushd build
 
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake ${CMAKE_ARGS} $SRC_DIR \
   -GNinja \
   -DWITH_PYTHON=ON \
